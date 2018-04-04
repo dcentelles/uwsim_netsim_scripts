5b8ecf9fac37614b6b1b6ff5459282be306a1175
dcentelles <centelld@uji.es>
2018-04-04 14:04:22 +0200

    add another example

---
+++ include/uwsim_netsim_scripts/MoveRobotsNetSimTracing.h
@@ -0,0 +1,27 @@
+#include <uwsim/NetSim.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <ros/publisher.h>
+
+namespace uwsim_netstim {
+
+using namespace uwsim;
+
+class MoveRobotsNetSimTracing : public NetSimTracing {
+public:
+  MoveRobotsNetSimTracing();
+  void ShowDistance(bool &lastPosValid, tf::Vector3 &lastPos,
+                    bool &peerPosValid, tf::Vector3 &peerPos, string path,
+                    ROSCommsDevicePtr dev, const tf::Vector3 &pos);
+  void ShowDistanceDev0(string path, ROSCommsDevicePtr dev,
+                        const tf::Vector3 &pos);
+  void ShowDistanceDev1(string path, ROSCommsDevicePtr dev,
+                        const tf::Vector3 &pos);
+  void Configure();
+  void DoRun();
+
+  tf::Vector3 dev0Pos, dev1Pos;
+  bool dev0PosValid = false, dev1PosValid = false;
+  ros::NodeHandle node;
+  ros::Publisher bluerov2Pub, bluerov2fPub;
+};
+}
---
+++ src/MoveRobotsNetSimTracing.cpp
@@ -0,0 +1,90 @@
+#include <class_loader/multi_library_class_loader.h>
+#include <functional>
+#include <uwsim_netsim_scripts/MoveRobotsNetSimTracing.h>
+
+namespace uwsim_netstim {
+
+MoveRobotsNetSimTracing::MoveRobotsNetSimTracing() : NetSimTracing() {
+  bluerov2Pub = node.advertise<geometry_msgs::TwistStamped>(
+      "/bluerov2/velocityCommand", 1);
+  bluerov2fPub = node.advertise<geometry_msgs::TwistStamped>(
+      "/bluerov2_f/velocityCommand", 1);
+}
+
+void MoveRobotsNetSimTracing::ShowDistance(bool &lastPosValid,
+                                           tf::Vector3 &lastPos,
+                                           bool &peerPosValid,
+                                           tf::Vector3 &peerPos, string path,
+                                           ROSCommsDevicePtr dev,
+                                           const tf::Vector3 &pos) {
+  lastPos = pos;
+  lastPosValid = true;
+  if (peerPosValid) {
+    double distance = lastPos.distance(peerPos);
+    Info("distance: {} meters", distance);
+  }
+}
+
+void MoveRobotsNetSimTracing::ShowDistanceDev0(string path,
+                                               ROSCommsDevicePtr dev,
+                                               const tf::Vector3 &pos) {
+  ShowDistance(dev0PosValid, dev0Pos, dev1PosValid, dev1Pos, path, dev, pos);
+}
+void MoveRobotsNetSimTracing::ShowDistanceDev1(string path,
+                                               ROSCommsDevicePtr dev,
+                                               const tf::Vector3 &pos) {
+  ShowDistance(dev1PosValid, dev1Pos, dev0PosValid, dev0Pos, path, dev, pos);
+}
+void MoveRobotsNetSimTracing::Configure() {
+  SetLogName("uwsim_netsim_scripts");
+  LogToFile("netsim_log");
+
+  //---------------------------------------------------------------------
+
+  ns3::Config::Connect(
+      "/ROSDeviceList/0/CourseChange",
+      ns3::MakeCallback(&MoveRobotsNetSimTracing::ShowDistanceDev0, this));
+  ns3::Config::Connect(
+      "/ROSDeviceList/1/CourseChange",
+      ns3::MakeCallback(&MoveRobotsNetSimTracing::ShowDistanceDev1, this));
+}
+
+void MoveRobotsNetSimTracing::DoRun() {
+  std::thread work([this]() {
+    geometry_msgs::TwistStamped msg;
+    msg.twist.linear.x = 0;
+    msg.twist.linear.y = 0;
+    msg.twist.linear.z = 0;
+    msg.twist.angular.x = 0;
+    msg.twist.angular.y = 0;
+    msg.twist.angular.z = 0;
+
+    double baseVelocity = 0.1;
+    int counter, its = 1000;
+
+    ros::Rate rate(20);
+    while (ros::ok()) {
+      msg.twist.linear.x = baseVelocity;
+
+      counter = 0;
+      while (counter < its) {
+        bluerov2Pub.publish(msg);
+        rate.sleep();
+        counter += 1;
+      }
+
+      msg.twist.linear.x = -baseVelocity;
+
+      counter = 0;
+      while (counter < its) {
+        bluerov2Pub.publish(msg);
+        rate.sleep();
+        counter += 1;
+      }
+    }
+  });
+  work.detach();
+}
+
+CLASS_LOADER_REGISTER_CLASS(MoveRobotsNetSimTracing, NetSimTracing)
+}
